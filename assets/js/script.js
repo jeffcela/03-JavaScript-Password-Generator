e98bb14941b6eb395d8757cbfda6ca238602f6ed
Jeff Cela <cela.j@labrador-company.com>
2021-07-11 12:27:12 -0400

    added reset css file, added in the variables for the program, the prompts for the user to read, the minimum criteria validation steps within the program, Copy to clipboard button (not active, just visible) WIP, added comments throughtout the js file for code indicators.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,44 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// These are the arrays of your input values that the generator will accept
+var splChar = ['@','%','+','\\','/',"'",'!','#','$','^','?',':',',',')','(','}','{',']','[','~','-','_','.',];
+var numChar = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var lcChar = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',];
+var ucChar = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z',];
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// This prompts the user to create password
+function getPasswordOptions() {
+  var length = parseInt(
+    prompt('How many characters would you like your password to contain? (select a value between 8-128'),
+    10
+  );
 
-  passwordText.value = password;
+  // The following if statements are validating the inputs to confirm it matches the programs criteria
+  if (Number.isNaN(length)) {
+    alert('Password length has to be a numeric vlaue');
+    return null;
+  }
 
-}
+  if (length < 8) {
+    alert('Password length must be minimum of 8 characters');
+    return null;
+  }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  if (length > 128) {
+    alert('Password length muct be less than 129 characters');
+    return null;
+  }
+
+  var hassplChar = confirm(
+    'Click OK to include special characters, cancel to opt out.'
+  );
+
+  var hasnumChar = confirm(
+    'Click OK to include numeric characters, cancel to opt out.'
+  );
+
+  var haslcChar = confirm(
+    'Click OK to include lowercase characters, cancel to opt out.'
+  );
+
+  var hasucChar = confirm(
+    'Click OK to include uppercase characters, cancel to opt out.'
+  );
